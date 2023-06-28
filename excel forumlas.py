a964c2b4e57d3a2e1a072e544905a82eae5f0952
Uncle Jesse <38791437+caryslone@users.noreply.github.com>
2023-06-28 09:40:47 -0600

    Add files via upload

--- excel forumlas.py
+++ excel forumlas.py
@@ -1,3 +1,12 @@
+####
+### Style color level formula for june without any of the if fcst strat or round functions.
+=SUMIF(bup[[style code]:[style code]],bup[@[pattern after sku 24]:[pattern after sku 24]],bup[[2023 units]:[2023 units]])*
+XLOOKUP(bup[@[superstyle]:[superstyle]],style[[style]:[style]],style[jun])*
+XLOOKUP(bup[@[size]:[size]],size[[size]:[size]],size[[percent]:[percent]])*
+(1+bup[@[growth override 23]:[growth override 23]])
+
+
+
 #Meateater forecast unit code--------------------------------
 ###Probably need to look at using previous month inventory as max acheivable as in FL codeblock below
 =IFERROR(ROUND(IF(bup[@[forecast strategy 23]:[forecast strategy 23]]="ytd.profile.cat",
@@ -30,18 +39,32 @@ IF(bup[@[forecast strategy 23]:[forecast strategy 23]]="opt.season",
 *XLOOKUP(bup[@[category]:[category]],cat.curve[[cat]:[cat]],cat.curve[may])*(1+bup[@[growth override 23]:[growth override 23]])))))))),0),0)
 
 
-#####---------------------
+
+#--------------------------------------------------------------------------#-------------------------------------------#----------------------
+###-------------------------------------------------------------------------------------------------------------------------------------------
+#--------------------------------------------------------------------------#-------------------------------------------#----------------------
+###-------------------------------------------------------------------------------------------------------------------------------------------
+
+
 ### First Lite Forecast Unit Code
 
 ### will need to manually update months in ytd.profile formula, it will not drag
 ##### applied to the month of May in 2022, YTD needs to sum to actualized months only
 ##### Need to manually update the sumif portion in the middle to 
 
-#----------------------------------- Build on May actuals and June forecast
-# matts update to include iventory try and lock all months in the ytd to see if it will auto increment
+#Build on May actuals and June forecast
+# matts update so that projected units cannot exceed last month EOH + Next months expected inventory
 
-=IF(
-IFERROR(ROUND(IF(bup[@[forecast strategy 23]:[forecast strategy 23]]="pattern.sa",
+#### 2023*** FL codeblock for 2023 units from June. Need to Manually update YTD fcsts code blocks
+=IF(IFERROR(ROUND(IF(bup[@[forecast strategy 23]:[forecast strategy 23]]="roll.avg.ly",
+AVERAGE([@[may 22 units]],[@[jun 22 units]],[@[jul 22 units]])*
+(1+bup[@[growth override 23]:[growth override 23]]),
+
+IF(bup[@[forecast strategy 23]:[forecast strategy 23]]="roll.avg.2y",
+SUM(([@[may 21 units]]*0.1133),([@[jun 21 units]]*0.1133),([@[jul 21 units]]*0.1133),
+([@[may 22 units]]*0.22),([@[jun 22 units]]*0.22),([@[jul 22 units]]*0.22))*(1+bup[@[growth override 23]:[growth override 23]]),
+
+IF(bup[@[forecast strategy 23]:[forecast strategy 23]]="pattern.sa",
 XLOOKUP(bup[@[pattern after sku 23]:[pattern after sku 23]],bup[[sku]:[sku]],bup[[2023 units]:[2023 units]])*
 XLOOKUP(bup[@[superstyle]:[superstyle]],style[[style]:[style]],style[jun])*
 (1+bup[@[growth override 23]:[growth override 23]]),
@@ -52,13 +75,17 @@ XLOOKUP(bup[@[pattern after sku 23]:[pattern after sku 23]],bup[[sku]:[sku]],bup
 XLOOKUP(bup[@[superstyle]:[superstyle]],style[[style]:[style]],style[jun])*
 (1+bup[@[growth override 23]:[growth override 23]]),
 
-IF(bup[@[forecast strategy 23]:[forecast strategy 23]]="roll.avg.ly",
-AVERAGE([@[may 22 units]],[@[jun 22 units]],[@[jul 22 units]])*
+IF(bup[@[forecast strategy 23]:[forecast strategy 23]]="style.color.sa",
+SUMIF(bup[[style code]:[style code]],bup[@[pattern after sku 23]:[pattern after sku 23]],bup[[2023 units]:[2023 units]])*
+XLOOKUP(bup[@[superstyle]:[superstyle]],style[[style]:[style]],style[jun])*
+XLOOKUP(bup[@[size]:[size]]&bup[@[superstyle]:[superstyle]],size[[size]:[size]]&size[[style]:[style]],size[[percent]:[percent]])*
 (1+bup[@[growth override 23]:[growth override 23]]),
 
-IF(bup[@[forecast strategy 23]:[forecast strategy 23]]="roll.avg.2y",
-SUM(([@[may 21 units]]*0.1133),([@[jun 21 units]]*0.1133),([@[jul 21 units]]*0.1133),
-([@[may 22 units]]*0.22),([@[jun 22 units]]*0.22),([@[jul 22 units]]*0.22))*(1+bup[@[growth override 23]:[growth override 23]]),
+IF(bup[@[forecast strategy 23]:[forecast strategy 23]]="style.color.dwo",
+SUMIF(bup[[style code]:[style code]],bup[@[pattern after sku 23]:[pattern after sku 23]],bup[[2022 units]:[2022 units]])*
+XLOOKUP(bup[@[superstyle]:[superstyle]],style[[style]:[style]],style[jun])*
+XLOOKUP(bup[@[size]:[size]]&bup[@[superstyle]:[superstyle]],size[[size]:[size]]&size[[style]:[style]],size[[percent]:[percent]])*
+(1+bup[@[growth override 23]:[growth override 23]]),
 
 IF(bup[@[forecast strategy 23]:[forecast strategy 23]]="opt.season",
 (bup[@[2022 units]:[2022 units]]/bup[@[2022 vol ach]:[2022 vol ach]])*
@@ -73,15 +100,20 @@ XLOOKUP(bup[@[superstyle]:[superstyle]],style[[style]:[style]],style[jun])*
 IF(bup[@[forecast strategy 23]:[forecast strategy 23]]="ytd.profile.subcat",
 SUM((bup[@[jan 23 units]:[may 23 units]]))/(XLOOKUP(bup[@[sub category ]:[sub category ]],ytd[[ytd]:[ytd]],ytd[may]))*
 XLOOKUP(bup[@[sub category ]:[sub category ]],style[[style]:[style]],style[jun])*
-(1+bup[@[growth override 23]:[growth override 23]])))))))),0),"shit")
+(1+bup[@[growth override 23]:[growth override 23]])))))))))),0),"shit")
 
-#Adjustment for max inventory level based on prevoius months eoh and next months on order. need to adjust may if not dragging accross
->[@[may 23 inv]]+SUMIFS('on order'!$G:$G,'on order'!$C:$C,[@[sku]:[sku]],'on order'!$O:$O,IP$7,'on order'!$P:$P,IP$6),
-[@[may 23 inv]]+SUMIFS('on order'!$G:$G,'on order'!$C:$C,[@[sku]:[sku]],'on order'!$O:$O,IP$7,'on order'!$P:$P,IP$6),
-                       
-#mirrors the above codeblock, completes the if < last month inventory and current month on order
+>[@[may 23 inv]]+SUMIFS('on order'!$G:$G,'on order'!$C:$C,bup[@[sku]:[sku]],'on order'!$O:$O,IT$7,'on order'!$P:$P,IT$6),
+[@[may 23 inv]]+SUMIFS('on order'!$G:$G,'on order'!$C:$C,bup[@[sku]:[sku]],'on order'!$O:$O,IT$7,'on order'!$P:$P,IT$6),
 
-IFERROR(ROUND(IF(bup[@[forecast strategy 23]:[forecast strategy 23]]="pattern.sa",
+IFERROR(ROUND(IF(bup[@[forecast strategy 23]:[forecast strategy 23]]="roll.avg.ly",
+AVERAGE([@[may 22 units]],[@[jun 22 units]],[@[jul 22 units]])*
+(1+bup[@[growth override 23]:[growth override 23]]),
+
+IF(bup[@[forecast strategy 23]:[forecast strategy 23]]="roll.avg.2y",
+SUM(([@[may 21 units]]*0.1133),([@[jun 21 units]]*0.1133),([@[jul 21 units]]*0.1133),
+([@[may 22 units]]*0.22),([@[jun 22 units]]*0.22),([@[jul 22 units]]*0.22))*(1+bup[@[growth override 23]:[growth override 23]]),
+
+IF(bup[@[forecast strategy 23]:[forecast strategy 23]]="pattern.sa",
 XLOOKUP(bup[@[pattern after sku 23]:[pattern after sku 23]],bup[[sku]:[sku]],bup[[2023 units]:[2023 units]])*
 XLOOKUP(bup[@[superstyle]:[superstyle]],style[[style]:[style]],style[jun])*
 (1+bup[@[growth override 23]:[growth override 23]]),
@@ -92,13 +124,17 @@ XLOOKUP(bup[@[pattern after sku 23]:[pattern after sku 23]],bup[[sku]:[sku]],bup
 XLOOKUP(bup[@[superstyle]:[superstyle]],style[[style]:[style]],style[jun])*
 (1+bup[@[growth override 23]:[growth override 23]]),
 
-IF(bup[@[forecast strategy 23]:[forecast strategy 23]]="roll.avg.ly",
-AVERAGE([@[may 22 units]],[@[jun 22 units]],[@[jul 22 units]])*
+IF(bup[@[forecast strategy 23]:[forecast strategy 23]]="style.color.sa",
+SUMIF(bup[[style code]:[style code]],bup[@[pattern after sku 23]:[pattern after sku 23]],bup[[2023 units]:[2023 units]])*
+XLOOKUP(bup[@[superstyle]:[superstyle]],style[[style]:[style]],style[jun])*
+XLOOKUP(bup[@[size]:[size]]&bup[@[superstyle]:[superstyle]],size[[size]:[size]]&size[[style]:[style]],size[[percent]:[percent]])*
 (1+bup[@[growth override 23]:[growth override 23]]),
 
-IF(bup[@[forecast strategy 23]:[forecast strategy 23]]="roll.avg.2y",
-SUM(([@[may 21 units]]*0.1133),([@[jun 21 units]]*0.1133),([@[jul 21 units]]*0.1133),
-([@[may 22 units]]*0.22),([@[jun 22 units]]*0.22),([@[jul 22 units]]*0.22))*(1+bup[@[growth override 23]:[growth override 23]]),
+IF(bup[@[forecast strategy 23]:[forecast strategy 23]]="style.color.dwo",
+SUMIF(bup[[style code]:[style code]],bup[@[pattern after sku 23]:[pattern after sku 23]],bup[[2022 units]:[2022 units]])*
+XLOOKUP(bup[@[superstyle]:[superstyle]],style[[style]:[style]],style[jun])*
+XLOOKUP(bup[@[size]:[size]]&bup[@[superstyle]:[superstyle]],size[[size]:[size]]&size[[style]:[style]],size[[percent]:[percent]])*
+(1+bup[@[growth override 23]:[growth override 23]]),
 
 IF(bup[@[forecast strategy 23]:[forecast strategy 23]]="opt.season",
 (bup[@[2022 units]:[2022 units]]/bup[@[2022 vol ach]:[2022 vol ach]])*
@@ -113,11 +149,65 @@ XLOOKUP(bup[@[superstyle]:[superstyle]],style[[style]:[style]],style[jun])*
 IF(bup[@[forecast strategy 23]:[forecast strategy 23]]="ytd.profile.subcat",
 SUM((bup[@[jan 23 units]:[may 23 units]]))/(XLOOKUP(bup[@[sub category ]:[sub category ]],ytd[[ytd]:[ytd]],ytd[may]))*
 XLOOKUP(bup[@[sub category ]:[sub category ]],style[[style]:[style]],style[jun])*
-(1+bup[@[growth override 23]:[growth override 23]])))))))),0),"shit"))
+(1+bup[@[growth override 23]:[growth override 23]])))))))))),0),"shit"))
 
-####--- Completed unit fcst codeblock for FL fcst workbook
 
-### Indiviudal formulas for fcst workbook
+
+#--------------------------------------------------------------------------#-------------------------------------------#----------------------
+###-------------------------------------------------------------------------------------------------------------------------------------------
+#--------------------------------------------------------------------------#-------------------------------------------#----------------------
+###-------------------------------------------------------------------------------------------------------------------------------------------
+
+
+#-- 2024***FL workbook forumlas for 2024
+=IFERROR(ROUND(IF(bup[@[forecast strategy 24]:[forecast strategy 24]]="roll.avg.ly",
+AVERAGE([@[dec 22 units]],[@[jan 23 units]],[@[feb 23 units]])*(1+bup[@[growth override 24]:[growth override 24]]),
+
+IF(bup[@[forecast strategy 24]:[forecast strategy 24]]="roll.avg.2y",
+SUM(([@[dec 21 units]]*0.1133),([@[jan 22 units]]*0.1133),([@[feb 22 units]]*0.1133),
+([@[dec 22 units]]*0.22),([@[jan 23 units]]*0.22),([@[feb 23 units]]*0.22))*(1+bup[@[growth override 24]:[growth override 24]]),
+
+IF(bup[@[forecast strategy 24]:[forecast strategy 24]]="pattern.sa",
+XLOOKUP(bup[@[pattern after sku 24]:[pattern after sku 24]],bup[[sku]:[sku]],bup[[2024 units]:[2024 units]])*
+XLOOKUP(bup[@[superstyle]:[superstyle]],style[[style]:[style]],style[jan])*(1+bup[@[growth override 24]:[growth override 24]]),
+
+IF(bup[@[forecast strategy 24]:[forecast strategy 24]]="pattern.dwo",
+XLOOKUP(bup[@[pattern after sku 24]:[pattern after sku 24]],bup[[sku]:[sku]],bup[[2023 units]:[2023 units]])/
+XLOOKUP(bup[@[pattern after sku 24]:[pattern after sku 24]],bup[[sku]:[sku]],bup[[2023 vol ach]:[2023 vol ach]])*
+XLOOKUP(bup[@[superstyle]:[superstyle]],style[[style]:[style]],style[jan])*(1+bup[@[growth override 24]:[growth override 24]]),
+
+IF(bup[@[forecast strategy 24]:[forecast strategy 24]]="style.color.sa",
+SUMIF(bup[[style code]:[style code]],bup[@[pattern after sku 24]:[pattern after sku 24]],bup[[2024 units]:[2024 units]])*
+XLOOKUP(bup[@[superstyle]:[superstyle]],style[[style]:[style]],style[jan])*
+XLOOKUP(bup[@[size]:[size]]&bup[@[superstyle]:[superstyle]],size[[size]:[size]]&size[[style]:[style]],size[[percent]:[percent]])*
+XLOOKUP(bup[@[color pattern]:[color pattern]]&bup[@[superstyle]:[superstyle]],color[[color]:[color]]&color[[style]:[style]],color[[percent]:[percent]])*(1+bup[@[growth override 24]:[growth override 24]]),
+
+IF(bup[@[forecast strategy 24]:[forecast strategy 24]]="style.color.dwo",
+SUMIF(bup[[style code]:[style code]],bup[@[pattern after sku 24]:[pattern after sku 24]],bup[[2023 units]:[2023 units]])*
+XLOOKUP(bup[@[superstyle]:[superstyle]],style[[style]:[style]],style[jan])*
+XLOOKUP(bup[@[size]:[size]]&bup[@[superstyle]:[superstyle]],size[[size]:[size]]&size[[style]:[style]],size[[percent]:[percent]])*
+XLOOKUP(bup[@[color pattern]:[color pattern]]&bup[@[superstyle]:[superstyle]],color[[color]:[color]]&color[[style]:[style]],color[[percent]:[percent]])*(1+bup[@[growth override 24]:[growth override 24]]),
+
+IF(bup[@[forecast strategy 24]:[forecast strategy 24]]="opt.season",
+(bup[@[2023 units]:[2023 units]]/bup[@[2023 vol ach]:[2023 vol ach]])*
+XLOOKUP(bup[@[superstyle]:[superstyle]],style[[style]:[style]],style[jan])*(1+bup[@[growth override 24]:[growth override 24]]),
+
+IF(bup[@[forecast strategy 24]:[forecast strategy 24]]="ytd.profile.style",
+SUM((bup[@[jan 24 units]:[jan 24 units]]))/(XLOOKUP(bup[@[superstyle]:[superstyle]],ytd[[ytd]:[ytd]],ytd[jan]))*
+XLOOKUP(bup[@[superstyle]:[superstyle]],style[[style]:[style]],style[jan])*(1+bup[@[growth override 24]:[growth override 24]]),
+
+IF(bup[@[forecast strategy 24]:[forecast strategy 24]]="ytd.profile.subcat",
+SUM((bup[@[jan 24 units]:[jan 24 units]]))/(XLOOKUP(bup[@[sub category ]:[sub category ]],ytd[[ytd]:[ytd]],ytd[jan]))*
+XLOOKUP(bup[@[sub category ]:[sub category ]],style[[style]:[style]],style[jan])*(1+bup[@[growth override 24]:[growth override 24]])))))))))),0),"shit")
+
+
+#--------------------------------------------------------------------------#-------------------------------------------#----------------------
+###-------------------------------------------------------------------------------------------------------------------------------------------
+#--------------------------------------------------------------------------#-------------------------------------------#----------------------
+###-------------------------------------------------------------------------------------------------------------------------------------------
+###-----------------------------------------------------------------------------------------------------------------------------------                      
+
+## Indiviudal formulas for fcst workbook
 #FL 1 year rolling average no weights
 =IF(bup[@[forecast strategy 23]:[forecast strategy 23]]="roll.avg.ly",
 AVERAGE([@[apr 22 units]],[@[may 22 units]],[@[jun 22 units]])*
@@ -162,30 +252,36 @@ XLOOKUP(bup[@[superstyle]:[superstyle]],style[[style]:[style]],style[may])*
 
 
 #logowear
-IF(bup[@[forecast strategy 23]:[forecast strategy 23]]="logowear",
+=IF(bup[@[forecast strategy 23]:[forecast strategy 23]]="logowear",
 ((bup[@[on order]:[on order]]+bup[@[on hand]:[on hand]])*0.8)*XLOOKUP(bup[@[superstyle]:[superstyle]],style[[style]:[style]],style[may])*
-(1+bup[@[growth override 23]:[growth override 23]]),
+(1+bup[@[growth override 23]:[growth override 23]])),
 
+#style.color.sa
+=IF(bup[@[forecast strategy 24]:[forecast strategy 24]]="style.color.sa",
+SUMIF(bup[[style code]:[style code]],bup[@[pattern after sku 24]:[pattern after sku 24]],bup[[2024 units]:[2024 units]])*
+XLOOKUP(bup[@[superstyle]:[superstyle]],style[[style]:[style]],style[jan])*
+XLOOKUP(bup[@[size]:[size]]&bup[@[superstyle]:[superstyle]],size[[size]:[size]]&size[[style]:[style]],size[[percent]:[percent]])*
+XLOOKUP(bup[@[color pattern]:[color pattern]]&bup[@[superstyle]:[superstyle]],color[[color]:[color]]&color[[style]:[style]],color[[percent]:[percent]])*
+(1+bup[@[growth override 24]:[growth override 24]])),
 
-#roll.style FL [still validating]
+#style.color.dwo
+=IF(bup[@[forecast strategy 24]:[forecast strategy 24]]="style.color.dwo",
+SUMIF(bup[[style code]:[style code]],bup[@[pattern after sku 24]:[pattern after sku 24]],bup[[2023 units]:[2023 units]])*
+XLOOKUP(bup[@[superstyle]:[superstyle]],style[[style]:[style]],style[jan])*
+XLOOKUP(bup[@[size]:[size]]&bup[@[superstyle]:[superstyle]],size[[size]:[size]]&size[[style]:[style]],size[[percent]:[percent]])*
+XLOOKUP(bup[@[color pattern]:[color pattern]]&bup[@[superstyle]:[superstyle]],color[[color]:[color]]&color[[style]:[style]],color[[percent]:[percent]])*
+(1+bup[@[growth override 24]:[growth override 24]])),
 
+#Adjustment for max inventory level based on prevoius months eoh and next months on order. need to adjust may if not dragging accross
+>[@[may 23 inv]]+SUMIFS('on order'!$G:$G,'on order'!$C:$C,[@[sku]:[sku]],'on order'!$O:$O,IP$7,'on order'!$P:$P,IP$6),
+[@[may 23 inv]]+SUMIFS('on order'!$G:$G,'on order'!$C:$C,[@[sku]:[sku]],'on order'!$O:$O,IP$7,'on order'!$P:$P,IP$6),
 
-=IF(bup[@[forecast strategy 23]:[forecast strategy 23]]="roll.style",
-=SUMIF(bup[[superstyle]:[superstyle]],bup[@[superstyle]:[superstyle]],[may 22 units])/bup[@[style count]:[style count]]*
-XLOOKUP(bup[@[gender]:[gender]]&bup[@[sub category ]:[sub category ]]&bup[@[size]:[size]],size[[gender]:[gender]]&size[[name]:[name]]&size[[size]:[size]],size[[percent]:[percent]])*
-XLOOKUP(bup[@[gender]:[gender]]&bup[@[sub category ]:[sub category ]]&bup[@[color pattern]:[color pattern]],color[[gender]:[gender]]&color[[name]:[name]]&color[[color]:[color]],color[[percent]:[percent]])
-
-
-=SUMIF(bup[[superstyle]:[superstyle]],bup[@[superstyle]:[superstyle]],[may 22 units])*
-XLOOKUP(bup[@[gender]:[gender]]&bup[@[sub category ]:[sub category ]]&bup[@[size]:[size]],size[[gender]:[gender]]&size[[name]:[name]]&size[[size]:[size]],size[[percent]:[percent]])*
-XLOOKUP(bup[@[gender]:[gender]]&bup[@[sub category ]:[sub category ]]&bup[@[color pattern]:[color pattern]],color[[gender]:[gender]]&color[[name]:[name]]&color[[color]:[color]],color[[percent]:[percent]])
-
-
-=SUMIF(bup[[superstyle]:[superstyle]],bup[@[superstyle]:[superstyle]],[nov 22 units])*
-XLOOKUP(bup[@[gender]:[gender]]&bup[@[sub category ]:[sub category ]]&bup[@[size]:[size]],size[[gender]:[gender]]&size[[name]:[name]]&size[[size]:[size]],size[[percent]:[percent]])*
-XLOOKUP(bup[@[gender]:[gender]]&bup[@[sub category ]:[sub category ]]&bup[@[color pattern]:[color pattern]],color[[gender]:[gender]]&color[[name]:[name]]&color[[color]:[color]],color[[percent]:[percent]])
-
------------------------------------------------------------------
+#--------------------------------------------------------------------------#-------------------------------------------#----------------------
+###-------------------------------------------------------------------------------------------------------------------------------------------
+#--------------------------------------------------------------------------#-------------------------------------------#----------------------
+###-------------------------------------------------------------------------------------------------------------------------------------------
+###-----------------------------------------------------------------------------------------------------------------------------------   
+#additional code blocks not related to unit forecasting
 
 #forecast trigger
 =IF([@status]="DWO", "dwo",
